c648a15a3d276051109ed06ac3ad03047a9df7b8
carriexu24 <cxu24@usfca.edu>
2018-12-26 22:19:59 -0800

    change code

--- server.py
+++ server.py
@@ -1,74 +1,40 @@
-"""
-A server that responds with two pages, one showing the most recent
-100 tweets for given user and the other showing the followers
-of a given user (sorted by the number of followers those users have).
-
-For authentication purposes, the server takes a commandline argument
-that indicates the file containing Twitter data in a CSV file format:
-
-    consumer_key, consumer_secret, access_token, access_token_secret
-
-For example, I pass in my secrets via file name:
-
-    /Users/parrt/Dropbox/licenses/twitter.csv
-
-Please keep in mind the limits imposed by the twitter API:
-
-    https://dev.twitter.com/rest/public/rate-limits
-
-For example, you can only do 15 follower list fetches per
-15 minute window, but you can do 900 user timeline fetches.
-"""
-import sys
 from flask import Flask, render_template
-from tweetie import *
-from colour import Color
+from doc2vec import *
+import sys
 
-from numpy import median
+# Launch with
+#
+# gunicorn -D --threads 4 -b 0.0.0.0:5000 --access-logfile server.log --timeout 60 server:app glove.6B.300d.txt bbc
 
 app = Flask(__name__)
 
-def add_color(tweets):
+@app.route("/")
+def articles():
+    """Show a list of article titles"""
+    return render_template("articles.html", articles = articles)
+
+
+
+@app.route("/article/<topic>/<filename>")
+def article(topic,filename):
     """
-    Given a list of tweets, one dictionary per tweet, add
-    a "color" key to each tweets dictionary with a value
-    containing a color graded from red to green. Pure red
-    would be for -1.0 sentiment score and pure green would be for
-    sentiment score 1.0.
-
-    Use colour.Color to get 100 color values in the range
-    from red to green. Then convert the sentiment score from -1..1
-    to an index from 0..100. That index gives you the color increment
-    from the 100 gradients.
-
-    This function modifies the dictionary of each tweet. It lives in
-    the server script because it has to do with display not collecting
-    tweets.
+    Show an article with relative path filename. Assumes the BBC structure of
+    topic/filename.txt so our URLs follow that.
     """
-    colors = list(Color("red").range_to(Color("green"), 100))
-    for t in tweets:
-        score = t['score']
-        t['color'] =colors[int((score+1) * 49.5)]
-    return tweets
+    for a in articles:
+        if topic + "/" + filename == a[0]:
+            article = a
+            break
 
-@app.route("/<name>")
-def tweets(name):
-    "Display the tweets for a screen name color-coded by sentiment score"
-    tweets = add_color(fetch_tweets(api, name)['tweets'])
-    score = sorted([tweet['score'] for tweet in tweets])[(len(tweets) + 1) / 2]
-    return render_template("tweets.html", tweets=tweets, score=score, name=name)
+    seealso = recommended(article, articles, 5)
+
+    return render_template("article.html", article = article, seealso = seealso)
 
 
-@app.route("/following/<name>")
-def following(name):
-    """
-    Display the list of users followed by a screen name, sorted in
-    reverse order by the number of followers of those users.
-    """
-    following = fetch_following(api, name)
-    return render_template("following.html", following=following, name=name)
-
+# initialization
 i = sys.argv.index('server:app')
-twitter_auth_filename = sys.argv[i+1] # e.g., "/Users/parrt/Dropbox/licenses/twitter.csv"
-api = authenticate(twitter_auth_filename)
+glove_filename = sys.argv[i+1]
+articles_dirname = sys.argv[i+2]
 
+gloves = load_glove(glove_filename)
+articles = load_articles(articles_dirname, gloves)
